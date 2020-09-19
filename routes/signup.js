5ce14689f864dc0bbe0c0ea9275569c2082fb040
Mimi Tran <7123mimit@gmail.com>
2020-09-19 17:48:21 +1000

    fix routes

--- routes/signup.js
+++ routes/signup.js
@@ -3,13 +3,9 @@ const router = express.Router();
 const signup_controller = require('../controllers/signupController');
 
 /* GET sign up page. */
-router.get('/', function (req, res, next) {
-    signup_controller.user_create_get;
-});
+router.get('/', signup_controller.user_create_get);
 
 /* POST sign up page. */
-router.post('/', function (req, res, next) {
-    signup_controller.user_create_post;
-});
+router.post('/', signup_controller.user_create_post);
 
 module.exports = router;
