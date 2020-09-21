e220a0611c77047a24bdf0f6529829c2da3d62b4
Mimi Tran <7123mimit@gmail.com>
2020-09-21 15:39:42 +1000

    update index route to use controller

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const index_controller = require('../controllers/indexController');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', index_controller.index_get);
 
 module.exports = router;
