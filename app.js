baec9bf45cac4ddc1f8135cdb9f181c98a245242
Mimi Tran <7123mimit@gmail.com>
2020-09-21 20:20:57 +1000

    prepare for deployment

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
 require('dotenv').config();
 
 //connect to database
@@ -33,6 +34,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(helmet());
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }));
