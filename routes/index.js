9697b624edc11143f11b7a1629436861859f7500
Mimi Tran <7123mimit@gmail.com>
2020-09-21 19:36:47 +1000

    add route for deleting messages

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,7 @@ const index_controller = require('../controllers/indexController');
 /* GET home page. */
 router.get('/', index_controller.index_get);
 
+/* GET home page. */
+router.post('/', index_controller.index_delete);
+
 module.exports = router;
